b3c28bcc6f011c08f0214bb1489edfe813c7b4d3
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-17 16:53:40 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,5 +2,5 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attribute_hash)
-    attribute_hash.each { |key, value| self.send() }
+    attribute_hash.each { |key, value| self.send("#{key}=", value) }
 end
